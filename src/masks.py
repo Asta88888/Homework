24aa7638da06bbce73bfba65317d64fc2f3537ce
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-01-23 14:49:28 +0300

    saving first homework

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[int]) -> Union[str]:
+    """Функция получает номер карты и возвращает ее маску"""
+    card_number_str = str(card_number)
+    card_mask = card_number_str[0:4], card_number_str[4:6] + "**", "****", card_number_str[-4:]
+    return " ".join(card_mask)
+
+
+def get_mask_account(account_number: Union[int]) -> Union[str]:
+    """Функция получает номер аккаунта и возвращает его маску"""
+    account_number_str = str(account_number)
+    account_mask = "**", account_number_str[-4:]
+    return "".join(account_mask)
+
+
+print(get_mask_card_number(7000792289606361))
+print(get_mask_account(73654108430135874305))
